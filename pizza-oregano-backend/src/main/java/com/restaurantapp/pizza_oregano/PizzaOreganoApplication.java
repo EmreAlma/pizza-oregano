673fd77d880bbd5e6e9aa5fa76f19bba427b6ad4
Emre Alma <emre@iMac-von-Emre.local>
2025-06-16 22:16:33 +0200

    Add backend (Spring Boot) project

---
+++ pizza-oregano-backend/src/main/java/com/restaurantapp/pizza_oregano/PizzaOreganoApplication.java
@@ -0,0 +1,13 @@
+package com.restaurantapp.pizza_oregano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaOreganoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaOreganoApplication.class, args);
+	}
+
+}
---
+++ pizza-oregano-backend/src/test/java/com/restaurantapp/pizza_oregano/PizzaOreganoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurantapp.pizza_oregano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaOreganoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
